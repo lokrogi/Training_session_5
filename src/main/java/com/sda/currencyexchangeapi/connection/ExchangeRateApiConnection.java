7a590b2896f9cc2d94ddc7ea4d35855412d2a0f2
igor <kolodziejigor@gmail.com>
2022-04-12 19:42:32 +0200

    first endpoint for currency rates

---
+++ src/main/java/com/sda/currencyexchangeapi/connection/ExchangeRateApiConnection.java
@@ -0,0 +1,35 @@
+package com.sda.currencyexchangeapi.connection;
+
+import org.json.JSONObject;
+import org.springframework.stereotype.Component;
+import org.springframework.web.util.UriBuilder;
+
+import java.io.IOException;
+import java.net.*;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+@Component
+public class ExchangeRateApiConnection {
+
+    private static final String stringUrl = "https://api.exchangerate.host/latest";
+
+    public JSONObject getLatestCurrencyExchange(String base, String target) throws IOException, URISyntaxException, InterruptedException {
+
+        String strUrlWithParams = stringUrl + "?base=" + base + "&symbols=" + target;
+
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(new URI(strUrlWithParams))
+                .version(HttpClient.Version.HTTP_2)
+                .GET()
+                .build();
+
+        HttpResponse<String> response = HttpClient.newHttpClient()
+                .send(request, HttpResponse.BodyHandlers.ofString());
+
+        return new JSONObject(response.body());
+
+    }
+}
