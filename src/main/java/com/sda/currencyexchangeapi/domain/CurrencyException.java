c2af7d403dcf434a541d1b2f8d4b556851cc0e81
igor <kolodziejigor@gmail.com>
2022-04-19 10:29:11 +0200

    added GlobalExceptionHandler

---
+++ src/main/java/com/sda/currencyexchangeapi/domain/CurrencyException.java
@@ -0,0 +1,7 @@
+package com.sda.currencyexchangeapi.domain;
+
+public class CurrencyException extends RuntimeException{
+    public CurrencyException(String message) {
+        super(message);
+    }
+}
