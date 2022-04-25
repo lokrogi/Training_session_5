4104eff6efaf1084ee1549985db45b09f3aded65
igor <kolodziejigor@gmail.com>
2022-04-25 15:15:36 +0200

    Integration tests for currencies, DateFormatException

--- src/main/java/com/sda/currencyexchangeapi/controller/RestApiController.java
+++ src/main/java/com/sda/currencyexchangeapi/controller/RestApiController.java
@@ -7,10 +7,12 @@ import com.sda.currencyexchangeapi.model.CurrencyDto;
 import com.sda.currencyexchangeapi.model.GoldDto;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.net.http.HttpResponse;
 import java.time.LocalDate;
 
 
@@ -28,28 +30,29 @@ public class RestApiController {
     }
 
     @GetMapping("api/latest")
-    public CurrencyDto getLatestCurrencyRate(
+    public ResponseEntity<CurrencyDto> getLatestCurrencyRate(
             @RequestParam(name = "base") String base,
             @RequestParam(name = "target") String target) {
 
         String date = String.valueOf(LocalDate.now());
-        return currencyService.getLatestCurrencyRate(base, target, date);
+
+        return ResponseEntity.ok(currencyService.getLatestCurrencyRate(base, target, date));
     }
 
     @GetMapping("api/historical")
-    public CurrencyDto getHistoricalCurrencyRate(
+    public ResponseEntity<CurrencyDto> getHistoricalCurrencyRate(
             @RequestParam(name = "base") String base,
             @RequestParam(name = "target") String target,
             @RequestParam(name = "date") String date){
 
-        return currencyService.getLatestCurrencyRate(base, target, date);
+        return ResponseEntity.ok(currencyService.getLatestCurrencyRate(base, target, date));
     }
 
     @GetMapping("api/gold")
-    public GoldDto getGoldPrice(
+    public ResponseEntity<GoldDto> getGoldPrice(
             @RequestParam(name = "date") String date) {
 
-        return goldService.getGoldPrice(date);
+        return ResponseEntity.ok(goldService.getGoldPrice(date));
     }
 
 }
---
+++ src/main/java/com/sda/currencyexchangeapi/domain/DateFormatException.java
@@ -0,0 +1,7 @@
+package com.sda.currencyexchangeapi.domain;
+
+public class DateFormatException extends RuntimeException{
+    public DateFormatException(String message) {
+        super(message);
+    }
+}
